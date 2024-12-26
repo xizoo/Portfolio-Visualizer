512f28e69948ecbc695841ce6674f1a9088cf0b5
xizoo <toh.xianzong03@gmail.com>
2024-12-26 19:51:57 +0800

    improved fedrate calculator

--- resources/fedrate.py
+++ resources/fedrate.py
@@ -1,9 +1,38 @@
 from pandas_datareader import data as pdr
-import datetime
+import requests
+from datetime import datetime as dt
+import json
 
-start = datetime.datetime(2000, 1, 1)
-end = datetime.datetime(2023, 12, 31)
 
-# Fetch Fed Funds Rate from FRED
-fed_rate = pdr.DataReader('FEDFUNDS', 'fred', start, end)
-print(fed_rate.head())
+#Different rates that you can extract, or use your own
+treasury_1 = "DGS1"
+treasury_3 = "DGS3"
+treasury_5 = "DGS5"
+fed_rate = "EFFR"
+
+def fetch_rate(id):
+    # Define the API key and endpoint
+    api_key = '52c0724c62e67f6e8fae558c6a37fe5d'
+    series_id = id
+    start_date = '2000-01-01'
+    end_date = dt.today().strftime('%Y-%m-%d')
+    url = f'https://api.stlouisfed.org/fred/series/observations?series_id={series_id}&api_key={api_key}&file_type=json&observation_start={start_date}&observation_end={end_date}'
+
+    # Fetch data from FRED API
+    response = requests.get(url)
+    data = response.json()
+
+    # Check if 'observations' key is in the response
+    if 'observations' in data:
+        # Extract the latest fund rate for the 1-year treasury
+        observations = data['observations']
+        latest_observation = observations[-1]
+        latest_date = latest_observation['date']
+        latest_rate = latest_observation['value']
+        print(f"Latest Rate on {end_date}: {latest_rate}")
+        return latest_rate
+    else:
+        print("Error: 'observations' key not found in the response")
+        print(data)
+
+fetch_rate(fed_rate)
